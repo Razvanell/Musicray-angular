5768272a283f74eaebb013ba9b8987a6cc8e88a4
Razvanell <stanciurazvanell95@gmail.com>
2025-05-21 00:49:56 +0200

    Update build configuration and production API URL in environment settings

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiBaseUrl: 'http://localhost:8080/api'
+  apiBaseUrl: 'https://523d-2a02-8388-e539-9700-2475-ad2b-ef5-32ea.ngrok-free.app/api'
 };
